eb420f769f4d86609a50718e3dd9c7f8e32ce46e
uRodolfo <123848813+uRodolfo@users.noreply.github.com>
2024-05-15 17:59:29 -0300

    Update Main.java

--- src/test/java/Principal/Main.java
+++ src/test/java/Principal/Main.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Principal;
+import Principal.Conexao;
 
-/**
- *
- * @author robso
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        Conexao.consultarClientes();
+    }
 }
